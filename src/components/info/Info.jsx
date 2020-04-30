a159033571ce9eb2346a2975baf0b35374f82bae
Andrew Lor <andrewlor332@gmail.com>
2020-04-30 15:21:28 -0600

    update info box

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -1,23 +1,42 @@
 import React, { useState } from "react";
 import "./Info.sass";
+import CloseImage from "../../../public/close.png";
 
 const Info = React.memo(() => {
     const [showInfo, setShowInfo] = useState(false);
 
     return (
         <>
-            <div
-                className="icon"
-                onMouseEnter={() => setShowInfo(true)}
-                onMouseLeave={() => setShowInfo(false)}
-            >
+            <div className="info-icon" onClick={() => setShowInfo(!showInfo)}>
                 <p>?</p>
             </div>
             {showInfo ? (
-                <p className="info">
-                    Click and drag on the timeline on the left side of the screen to navigate
-                    quickly. Use the left and right arrow keys to nagivate precisely.
-                </p>
+                <>
+                    <div className="info-backdrop" />
+                    <div className="info-box">
+                        <h2>How to use</h2>
+                        <ul>
+                            <li>
+                                Click and drag on the timeline on the left side of the screen to
+                                navigate dates quickly
+                            </li>
+                            <li>Use the left and right arrow keys to nagivate dates precisely</li>
+                            <li>Use gear in the bottom right to filter divisions</li>
+                            <li>
+                                Click the pin beside a fighters name to highlight them, follow them
+                                through the rankings!
+                            </li>
+                            <li>Tip: You can click on a fighters name to look at their profile</li>
+                        </ul>
+                        <p className="icon-source">Icons source: https://icons8.com</p>
+                        <img
+                            className="exit"
+                            src={CloseImage}
+                            alt="Close"
+                            onClick={() => setShowInfo(!showInfo)}
+                        />
+                    </div>
+                </>
             ) : null}
         </>
     );
