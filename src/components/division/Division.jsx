1e4a367dbd0fb3e892ef2f85f796a30d38f7f7f7
Andrew Lor <andrewlor332@gmail.com>
2020-04-30 14:51:17 -0600

    add fighter highlight functionality

--- src/components/division/Division.jsx
+++ src/components/division/Division.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Division.sass";
-
+import PinImage from "../../../public/pin.png";
 const athleteUrl = (fighter) => {
     const fighterNameUrl = fighter.toLowerCase().replace(" ", "-");
     return `https://www.ufc.com/athlete/${fighterNameUrl}`;
@@ -9,17 +9,32 @@ const athleteUrl = (fighter) => {
 
 const isChamp = (rank) => rank == 0;
 
-const Division = React.memo(({ title, rankings }) => {
+const Division = React.memo(({ title, rankings, selectedFighters, setSelectedFighters }) => {
+    const isSelected = (fighter) => selectedFighters.includes(fighter);
+    const handlePinClick = (fighter) => () => {
+        const newSelectedFighters = isSelected(fighter)
+            ? selectedFighters.filter((d) => d !== fighter)
+            : selectedFighters.concat(fighter);
+        setSelectedFighters(newSelectedFighters);
+    };
+    const buildClassNames = (rank, fighter) => {
+        return `rank\
+            ${isChamp(rank) ? "champ" : ""}\
+            ${isSelected(fighter) ? "selected" : ""}
+        `;
+    };
+
     return (
         <div className="division">
             <p className="title">{title}</p>
             {rankings.map(({ rank, fighter }) => (
-                <div className={`rank ${rank == 0 ? "champ" : ""}`} key={fighter}>
+                <div className={buildClassNames(rank, fighter)} key={fighter}>
                     {!isChamp(rank) ? <span className="bold">{rank} </span> : null}
                     <a href={athleteUrl(fighter)} target="_blank">
                         {fighter}
                     </a>
                     {isChamp(rank) ? <p>Champion</p> : null}
+                    <img className="pin" src={PinImage} onClick={handlePinClick(fighter)} />
                 </div>
             ))}
         </div>
@@ -29,6 +44,8 @@ const Division = React.memo(({ title, rankings }) => {
 Division.propTypes = {
     title: PropTypes.string.isRequired,
     rankings: PropTypes.arrayOf(PropTypes.object).isRequired,
+    selectedFighters: PropTypes.arrayOf(PropTypes.string).isRequired,
+    setSelectedFighters: PropTypes.func.isRequired,
 };
 
 export default Division;
--- src/components/rankings/Rankings.jsx
+++ src/components/rankings/Rankings.jsx
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Division from "../division/Division";
 import "./Rankings.sass";
 import { formatDate } from "../../utils";
 
-const Rankings = React.memo(({ divisions, date, selectedDivisions }) => (
-    <>
-        <h1 className="date">UFC Athlete Rankings: {formatDate(date)}</h1>
-        <div className="rankings">
-            {Object.keys(divisions).map((division) =>
-                selectedDivisions.includes(division) ? (
-                    <Division key={division} title={division} rankings={divisions[division]} />
-                ) : null
-            )}
-        </div>
-    </>
-));
+const Rankings = React.memo(({ divisions, date, selectedDivisions }) => {
+    const [selectedFighters, setSelectedFighters] = useState([]);
+
+    return (
+        <>
+            <h1 className="date">UFC Athlete Rankings: {formatDate(date)}</h1>
+            <div className="rankings">
+                {Object.keys(divisions).map((division) =>
+                    selectedDivisions.includes(division) ? (
+                        <Division
+                            key={division}
+                            title={division}
+                            rankings={divisions[division]}
+                            selectedFighters={selectedFighters}
+                            setSelectedFighters={setSelectedFighters}
+                        />
+                    ) : null
+                )}
+            </div>
+        </>
+    );
+});
 
 Rankings.propTypes = {
     date: PropTypes.string.isRequired,
