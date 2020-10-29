fde1fddfc71f588853595789d6a66f133ab23cad
Andrew Lor <andrewlor332@gmail.com>
2020-10-29 17:06:00 -0600

    add move by 10 buttons

--- src/components/control-panel/ControlPanel.jsx
+++ src/components/control-panel/ControlPanel.jsx
@@ -5,12 +5,19 @@ import ChevronLeft from "../../../public/assets/img/chevron-left.png";
 import ChevronRight from "../../../public/assets/img/chevron-right.png";
 import Chevron2Left from "../../../public/assets/img/chevron-2-left.png";
 import Chevron2Right from "../../../public/assets/img/chevron-2-right.png";
+import Move10Left from "../../../public/assets/img/move-10-left.png";
+import Move10Right from "../../../public/assets/img/move-10-right.png";
 
 const ControlPanel = React.memo(({ index, setIndex, maxIndex }) => {
     return (
         <div className="control-panel">
             <img
                 src={Chevron2Left}
+                className={`clickable ${index === maxIndex ? "disabled" : ""}`}
+                onClick={() => setIndex(maxIndex)}
+            />
+            <img
+                src={Move10Left}
                 className={`clickable ${index === maxIndex ? "disabled" : ""}`}
                 onClick={() => setIndex(index + 10)}
             />
@@ -21,15 +28,18 @@ const ControlPanel = React.memo(({ index, setIndex, maxIndex }) => {
             />
             <img
                 src={ChevronRight}
-                className="clickable"
                 className={`clickable ${index === 0 ? "disabled" : ""}`}
                 onClick={() => setIndex(index - 1)}
             />
             <img
-                src={Chevron2Right}
-                className="clickable"
+                src={Move10Right}
                 className={`clickable ${index === 0 ? "disabled" : ""}`}
                 onClick={() => setIndex(index - 10)}
+            />
+            <img
+                src={Chevron2Right}
+                className={`clickable ${index === 0 ? "disabled" : ""}`}
+                onClick={() => setIndex(0)}
             />
         </div>
     );
